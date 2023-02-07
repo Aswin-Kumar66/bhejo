db643b30d105730d40e31efd2e86e4646c4e228a
Aswin-Kumar66 <77041105+Aswin-Kumar66@users.noreply.github.com>
2023-02-07 22:11:44 +0530

    base url

--- frontend/baseUrl.js
+++ frontend/baseUrl.js
@@ -1,2 +1,2 @@
-// export const baseUrl="https://bhejo.cyclic.app/api/";
-export const baseUrl="http://localhost:5000/api/";
+export const baseUrl="https://bhejo-r4he-api.onrender.com/api/";
+// export const baseUrl="http://localhost:5000/api/";
