c8f86bd514cd56e94e743f8dd55eed0d455678bc
Aswin-Kumar66 <77041105+Aswin-Kumar66@users.noreply.github.com>
2023-02-07 13:11:56 +0530

    updated baseurl

--- frontend/src/baseUrl.js
+++ frontend/src/baseUrl.js
@@ -1,2 +1,2 @@
-// export const baseUrl="https://bhejo.onrender.com/api/";
-export const baseUrl="http://localhost:5000/api/";
+export const baseUrl="https://bhejo-r4he.onrender.com/api/";
+// export const baseUrl="http://localhost:5000/api/";
